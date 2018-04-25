73728f65da8971704ed8803fa296e76177682b9d
tysonmonster101 <tyson.s250@stu.nebo.edu>
2018-04-25 14:17:59 -0600

    asdf

--- PortfolioSite/ActualSite/portfolioscript.js
+++ PortfolioSite/ActualSite/portfolioscript.js
@@ -1,5 +1,10 @@
+var imageNameAndDescription = {nameOfImage:[], subImages:[], description:[]};
+var imageDescriptionStuff = imageNameAndDescription[10];
+
+
 $(document).ready(function()
 {
+    /*imageNameAndDescription.nameOfImage[2] = "yeet";*/
     //alert("start");
     $("#trash").css({"background-color":"red"})
     //$("#trash").style.backgroundColor = "red";
