c28dc4adc187d5ca7027ba6792aa27b5a33b85de
tysonmonster101 <tyson.s250@stu.nebo.edu>
2018-04-27 14:16:51 -0600

    zsdf
    zsdf

--- PortfolioSite/ActualSite/portfolioscript.js
+++ PortfolioSite/ActualSite/portfolioscript.js
@@ -1,12 +1,19 @@
-var imageNameAndDescription = {nameOfImage:[], subImages:[], description:[]};
-var imageDescriptionStuff = imageNameAndDescription[10];
-
+var galleryImages;
 
 $(document).ready(function()
 {
-    /*imageNameAndDescription.nameOfImage[2] = "yeet";*/
-    //alert("start");
-    $("#trash").css({"background-color":"red"})
+    galleryImages = $(".gallery");
+    var incrementAmount = screen.width/galleryImages.length;
+    alert(screen.width);
+    alert(incrementAmount);
+    
+    for (var i = 0; i < galleryImages.length; i++)
+    {
+        $(galleryImages[i]).css("width","100px");
+        $(galleryImages[i]).css("zIndex",i.toString());
+        $(galleryImages[i]).css("left",(incrementAmount * i).toString());
+        //galleryImages[i].style.width = "10px";
+    }
     //$("#trash").style.backgroundColor = "red";
     alert("done");
 });
