3c0bfc1063812fe6d3f5d0616ba3a535251d3bf3
tysonmonster101 <tyson.s250@stu.nebo.edu>
2018-05-06 22:48:35 -0600

    added images
    got the images to resize and stuff just need sliding action

--- PortfolioSite/ActualSite/portfolioscript.js
+++ PortfolioSite/ActualSite/portfolioscript.js
@@ -1,29 +1,49 @@
 var galleryImages;
+var nameOfImage;
+var imageDescription;
+var namesOfImages = ["firstImage","secondImage","thirdImage","fourthImage","fifthImage","sixthImage","seventhImage"];
+var imageDescriptions = ["first description is awesome",
+                         "second description is awesome",
+                         "third description is awesome",
+                         "fourth description is awesome",
+                         "fifth description is awesome",
+                         "sixth description is awesome",
+                         "seventh description is awesome"];
+
 
 $(".gallery").click(function()
 {
-    var targetZIndex = $(this).css("z-index");
-    $(this).animate()
-    alert($(this).css("z-index"));
+    var  thisGallery = $(this);
+    thisGallery.css("width","250px");
+    thisGallery.css("height","250px");
+    for(var i=0; i < galleryImages.length; i++)
+    {
+        if($(galleryImages[i]).css("width") == "250px" && $(galleryImages[i]).attr("src") != $(thisGallery).attr("src"))
+        {
+            $(galleryImages[i]).css("width","150px");
+            $(galleryImages[i]).css("height","150px");
+        }
+        if($(galleryImages[i]).attr("src") == $(thisGallery).attr("src"))
+        {
+            $(nameOfImage).html(namesOfImages[i].toString());
+            $(imageDescription).html(imageDescriptions[i].toString());
+        }
+    }
 });
 
 $(document).ready(function()
 {
     galleryImages = $(".gallery");
+    nameOfImage = $("#nameOfImage");
+    imageDescription = $("#descriptionText");
     var incrementAmount = screen.width/galleryImages.length;
 
     for (var i = 0; i < galleryImages.length/2; i++)
     {
-        //$(galleryImages[i]).css("width","100px");
         $(galleryImages[i]).css("z-index",i);
-        //galleryImages[i].style.width = "10px";
     }
     for (var i = 0; i < galleryImages.length/2 + 1; i++)
     {
-        //$(galleryImages[i]).css("width","100px");
         $(galleryImages[galleryImages.length - i]).css("z-index",i);
-        //galleryImages[i].style.width = "10px";
     }
-    //$("#trash").style.backgroundColor = "red";
-    alert("done");
 });
